bfe7fd15cf6773712d7b42fc3796486a74bda06d
Evgeniy Taranov <je.taranov@yandex.ru>
2023-09-10 20:22:40 +0300

    Заменил проект фронтенда на корректный

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
@@ -9,19 +10,34 @@ import CurrentUserContext from '../contexts/CurrentUserContext';
 import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
 import AddPlacePopup from './AddPlacePopup';
+import auth from '../utils/Auth';
+import InfoTooltip from './InfoTooltip';
+import ProtectedRouteElement from "./ProtectedRoute";
+import Login from "./Login";
+import Register from "./Register";
+import successfulAttemptImage from "../images/succesful-auth.svg";
+import failedAttemptImage from "../images/failed-auth.svg";
 
 const App = () => {
     const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
     const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
     const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+    const [isConfirmationPopupOpen, setIsConfirmationPopupOpen] = useState(false);
+    const [registrationResult, setRegistrationResult] = useState(false);
+    const [isInfoTooltipOpen, setIsInfoTooltipOpen] = useState(false);
     const [selectedCard, setSelectedCard] = useState(null);
     const [currentUser, setCurrentUser] = useState(null);
     const [cards, setCards] = useState([]);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [email, setEmail] = useState('');
+    const navigate = useNavigate();
 
     const closeAllPopups = () => {
         setIsEditProfilePopupOpen(false);
         setIsAddPlacePopupOpen(false);
         setIsEditAvatarPopupOpen(false);
+        setIsInfoTooltipOpen(false);
+        setIsConfirmationPopupOpen(false);
         setSelectedCard(null);
     };
 
@@ -51,21 +67,61 @@ const App = () => {
         getCards();
     }, []);
 
-    const handleEditAvatarClick = () => {
-        setIsEditAvatarPopupOpen(true);
-    };
+    useEffect(() => {
+        handleTokenCheck();
+    }, []);
 
-    const handleEditProfileClick = () => {
+    function onEditProfile() {
         setIsEditProfilePopupOpen(true);
-    };
+    }
 
-    const handleAddPlaceClick = () => {
+    function onAddPlace() {
         setIsAddPlacePopupOpen(true);
-    };
+    }
+
+    function onEditAvatar() {
+        setIsEditAvatarPopupOpen(true);
+    }
 
     const handleCardClick = (card) => {
         setSelectedCard(card);
     };
+
+    function onConfirmationPopup() {
+        setIsConfirmationPopupOpen(true);
+    }
+
+    const handleInfoTooltipOpen = () => {
+        setIsInfoTooltipOpen(true);
+    };
+
+    function handleTokenCheck() {
+        if (localStorage.getItem('token')) {
+            const token = localStorage.getItem('token');
+            auth.checkToken(token)
+                .then((res) => {
+                    setLoggedIn(true);
+                    setEmail(res.data.email);
+                    navigate('/', { replace: true });
+                })
+                .catch((err) => {
+                    console.log('Ошибка:', err);
+                });
+        }
+    }
+
+    function handleLogin(loginData) {
+        auth.login(loginData.email, loginData.password)
+            .then((res) => {
+                localStorage.setItem('token', res.token);
+                handleTokenCheck();
+            })
+            .catch((err) => {
+                console.log('Ошибка...:', err);
+                setRegistrationResult(false);
+                handleInfoTooltipOpen();
+            });
+    }
 
     const handleCardLike = async (card) => {
         const isLiked = card.likes.some((like) => like._id === currentUser._id);
@@ -148,41 +204,94 @@ const App = () => {
             });
     };
 
+    function handleRegistration(registerData) {
+        auth.register(registerData.email, registerData.password)
+            .then((res) => {
+                navigate('/sign-in', { replace: true });
+                setRegistrationResult(true);
+                handleInfoTooltipOpen();
+            })
+            .catch((err) => {
+                console.log('Ошибка...:', err);
+                setRegistrationResult(false);
+                handleInfoTooltipOpen();
+            });
+    }
+
+    function handleExit() {
+        localStorage.removeItem('token');
+        setEmail('');
+        setLoggedIn(false);
+    }
+
     return (
-        <>
-            <CurrentUserContext.Provider value={currentUser}>
-                <div className="page">
-                    <Header />
-                    <Main
-                        cards={cards}
-                        onEditProfile={handleEditProfileClick}
-                        onAddPlace={handleAddPlaceClick}
-                        onEditAvatar={handleEditAvatarClick}
-                        onCardClick={handleCardClick}
-                        onCardLike={handleCardLike}
-                        onCardDislike={handleCardDislike}
-                        onCardDelete={handleCardDelete}
+        <CurrentUserContext.Provider value={currentUser}>
+            <div className="page">
+                <Header
+                    email={email}
+                    onExit={handleExit}
+                />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <ProtectedRouteElement
+                                element={Main}
+                                loggedIn={loggedIn}
+                                onAddPlace={onAddPlace}
+                                isAddPlacePopupOpen={isAddPlacePopupOpen}
+                                onEditProfile={onEditProfile}
+                                isEditProfilePopupOpen={isEditProfilePopupOpen}
+                                onEditAvatar={onEditAvatar}
+                                isEditAvatarPopupOpen={isEditAvatarPopupOpen}
+                                onCardClick={handleCardClick}
+                                onCardDelete={handleCardDelete}
+                                onCardLike={handleCardLike}
+                                onCardDislike={handleCardDislike}
+                                onConfirmationPopup={onConfirmationPopup}
+                                cards={cards}
+                            />
+                        }
                     />
-                    <Footer />
-                    <EditProfilePopup
-                        isOpen={isEditProfilePopupOpen}
-                        onClose={closeAllPopups}
-                        onUpdateUser={handleUpdateUser}
-                    />
-                    <EditAvatarPopup
-                        isOpen={isEditAvatarPopupOpen}
-                        onClose={closeAllPopups}
-                        onUpdateAvatar={handleUpdateAvatar}
-                    />
-                    <AddPlacePopup
-                        isOpen={isAddPlacePopupOpen}
-                        onClose={closeAllPopups}
-                        onAddPlace={handleAddPlaceSubmit}
-                    />
-                    <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-                </div>
-            </CurrentUserContext.Provider>
-        </>
+                    <Route path='/sign-up' element={
+                        <Register
+                            onSubmit={handleRegistration} />
+                    } />
+                    <Route path='/sign-in' element={
+                        <Login
+                            onSubmit={handleLogin} />
+                    } />
+                </Routes>
+                {loggedIn && <Footer />}
+                <EditProfilePopup
+                    isOpen={isEditProfilePopupOpen}
+                    onClose={closeAllPopups}
+                    onUpdateUser={handleUpdateUser}
+                />
+                <EditAvatarPopup
+                    isOpen={isEditAvatarPopupOpen}
+                    onClose={closeAllPopups}
+                    onUpdateAvatar={handleUpdateAvatar}
+                />
+                <AddPlacePopup
+                    isOpen={isAddPlacePopupOpen}
+                    onClose={closeAllPopups}
+                    onAddPlace={handleAddPlaceSubmit}
+                />
+                <InfoTooltip
+                    isOpen={isInfoTooltipOpen}
+                    onClose={closeAllPopups}
+                    result={registrationResult}
+                />
+                <PopupWithForm
+                    title="Уверены?"
+                    name="confirmation"
+                    isOpen={isConfirmationPopupOpen}
+                    onClose={closeAllPopups}
+                />
+                <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+            </div>
+        </CurrentUserContext.Provider>
     );
 };
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,49 @@
-import mestoWhiteLogo from '../images/mesto-white-logo.svg';
+import { Link, useLocation } from 'react-router-dom';
+import logoPath from '../images/mesto-white-logo.svg';
+import React from 'react';
 
-const Header = () => {
+function Header({ email, onExit }) {
+  const location = useLocation();
+
+  const HeaderNavLink = {
+    '/sign-in': {
+      linkTo: '/sign-up',
+      linkName: "Регистрация"
+    },
+    '/sign-up': {
+      linkTo: '/sign-in',
+      linkName: "Вход"
+    },
+    '/': {
+      linkTo: '/sign-in',
+      linkName: "Выход"
+    },
+  }
+
+  const RenderHeaderNavLink = (location) => {
+    if (location.pathname in HeaderNavLink) {
+      return HeaderNavLink[location.pathname];
+    } else {
+      return {
+        linkTo: '/',
+        linkName: "Назад"
+      }
+    }
+  }
+
+  const handleCkick = () => {
+    if (location.pathname === '/') {
+      onExit();
+    }
+  }
 
   return (
     <header className="header">
-      <img
-        src={mestoWhiteLogo}
-        alt="Белый логотип Место"
-        className="header__logo"
-      />
+      <img className="header__logo" alt="Белый логотип Mesto" src={logoPath} />
+      <span className="header__navigation">{email}<Link className="header__link" onClick={handleCkick}
+        to={RenderHeaderNavLink(location).linkTo}>{RenderHeaderNavLink(location).linkName}</Link></span>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
